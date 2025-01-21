834240606196419dd77039e025eadc17f72b4535
TJChiang <tjchiang8419@gmail.com>
2025-01-21 09:44:48 +0800

    fix pdf error for chinese fonts

--- resources/views/pdf/shipments.blade.php
+++ resources/views/pdf/shipments.blade.php
@@ -5,8 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $shipment_number }}</title>
     <style>
+        @font-face {
+            font-weight: normal;
+            font-family: 'Noto Sans TC';
+            src: url({{ storage_path('fonts/NotoSansTC/NotoSansTC-Regular.ttf') }}) format('truetype');
+            font-style: normal;
+        }
+        @font-face {
+            font-weight: bold;
+            font-family: 'Noto Sans TC';
+            src: url({{ storage_path('fonts/NotoSansTC/NotoSansTC-Bold.ttf') }}) format('truetype');
+            font-style: normal;
+        }
         body {
-            font-family: DejaVu Sans, sans-serif;
+            font-family: 'Noto Sans TC', DejaVu Sans, sans-serif;
             font-size: 12px;
         }
         table {
@@ -29,57 +41,80 @@
     <h2>訂單資訊</h2>
     <div>
         <table>
-            <tr>
-                <th>訂單號碼:</th>
-                <th>{{ $order_number}}</th>
-                <th>訂單日期:</th>
-                <th>{{ $order_date }}</th>
-            </tr>
-            <tr>
-                <th>收件人:</th>
-                <th>{{ $recipient_name }}</th>
-                <th>電話:</th>
-                <th>{{ $recipient_phone }}</th>
-            </tr>
-            <tr>
-                <th>收件地址:</th>
-                <th colspan="3">{{ $shipping_address }}</th>
-            </tr>
+            <tbody>
+                <tr>
+                    <th>訂單號碼:</th>
+                    <td>{{ $order_number}}</td>
+                    <th>訂單日期:</th>
+                    <td>{{ $order_date }}</td>
+                </tr>
+                <tr>
+                    <th>收件人:</th>
+                    <td>{{ $recipient_name }}</td>
+                    <th>電話:</th>
+                    <td>{{ $recipient_phone }}</td>
+                </tr>
+                <tr>
+                    <th>收件地址:</th>
+                    <td colspan="3">{{ $shipping_address }}</td>
+                </tr>
+            </tbody>
         </table>
     </div>
 
-    <h2>出貨單號碼: {{ $shipment_number }}</h2>
-    <p>配送: {{ $courier}}</p>
-    <p>追蹤碼: {{ $tracking_number }}</p>
-    <p>寄送時間: {{ $shipping_date }}</p>
-    <p>出貨狀態: {{ $status }}</p>
-
-    <table>
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>名稱</th>
-                <th>數量</th>
-                <th>單價</th>
-                <th>小計</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($order_items as $index => $item)
+    <h2>出貨單資訊</h2>
+    <div>
+        <table>
+            <tbody>
                 <tr>
-                    <td>{{ $index + 1 }}</td>
-                    <td>{{ $item['product_name'] }}</td>
-                    <td>{{ $item['price'] }}</td>
-                    <td>{{ $item['quantity'] }}</td>
-                    <td>{{ $item['subtotal'] }}</td>
+                    <th>出貨單號碼:</th>
+                    <td colspan="3">{{ $shipment_number }}</td>
                 </tr>
-            @endforeach
-            <tr>
-                <td colspan="3">總計</td>
-                <td>{{ $total_quantity }}</td>
-                <td>{{ $total_price }}</td>
-            </tr>
-        </tbody>
-    </table>
+                <tr>
+                    <th>配送:</th>
+                    <td>{{ $courier}}</td>
+                    <th>追蹤碼:</th>
+                    <td>{{ $tracking_number }}</td>
+                </tr>
+                <tr>
+                    <th>寄送時間:</th>
+                    <td>{{ $shipping_date }}</td>
+                    <th>出貨狀態:</th>
+                    <td>{{ $status }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <h3>商品資訊</h3>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>名稱</th>
+                    <th>數量</th>
+                    <th>單價</th>
+                    <th>小計</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($order_items as $index => $item)
+                    <tr>
+                        <td>{{ $index + 1 }}</td>
+                        <td>{{ $item['product_name'] }}</td>
+                        <td>{{ $item['price'] }}</td>
+                        <td>{{ $item['quantity'] }}</td>
+                        <td>{{ $item['subtotal'] }}</td>
+                    </tr>
+                @endforeach
+                <tr>
+                    <td colspan="3">總計</td>
+                    <td>{{ $total_quantity }}</td>
+                    <td>{{ $total_price }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
